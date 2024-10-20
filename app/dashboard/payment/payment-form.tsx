66a292a8a93d86921afe98675e610cec34e41aee
developeralamgirbd <developer.alamgirbd@gmail.com>
2024-10-20 22:31:29 +0600

    [Done] payment card update form test and test doc

--- app/dashboard/payment/payment-form.tsx
+++ app/dashboard/payment/payment-form.tsx
@@ -14,7 +14,6 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { toast } from "@/components/ui/use-toast";
-
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
@@ -24,138 +23,157 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-
-const items = [
-  {
-    id: "recents",
-    label: "Recents",
-  },
-  {
-    id: "home",
-    label: "Home",
-  },
-  {
-    id: "applications",
-    label: "Applications",
-  },
-  {
-    id: "desktop",
-    label: "Desktop",
-  },
-  {
-    id: "downloads",
-    label: "Downloads",
-  },
-  {
-    id: "documents",
-    label: "Documents",
-  },
-] as const;
+import {useState} from "react";
 
 const paymentFormSchema = z.object({
-  items: z.array(z.string()).refine((value) => value.some((item) => item), {
-    message: "You have to select at least one item.",
-  }),
+  name: z.string({message: "Name is required"}),
+  number: z.string().length(16, "Card number must be 16 digits"),
+  month: z.string({message: "Expiration month is required"}),
+  year: z.string({message: "Expiration year is required"}),
+  cvc: z.string().length(3, "CVC must be 3 digits"),
 });
 
 type PaymentFormValues = z.infer<typeof paymentFormSchema>;
 
-// This can come from your database or API.
-const defaultValues: Partial<PaymentFormValues> = {
-  items: ["recents", "home"],
-};
-
 export function PaymentForm() {
   const form = useForm<PaymentFormValues>({
     resolver: zodResolver(paymentFormSchema),
-    defaultValues,
   });
 
-  function onSubmit(data: PaymentFormValues) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    });
+    const [error, setError] = useState('')
+    const [successMsg, setSuccessMsg] = useState('')
+
+  async function onSubmit(data: PaymentFormValues) {
+
+    try {
+      const response = await fetch("/api/payment-card", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        const { error } = await response.json();
+        setError(error);
+      }else {
+          const { message } = await response.json();
+          setSuccessMsg(message)
+
+          toast({
+              title: "Success",
+              description: message,
+          });
+      }
+
+    } catch (error) {
+      toast({
+        title: "Error",
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        description: error.message,
+        variant: "destructive",
+      });
+    }
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-sm">
-        <FormField
-          control={form.control}
-          name="items"
-          render={() => (
-            <FormItem>
-              <div className="mb-4">
-                <FormLabel className="text-base">Card Information</FormLabel>
-                <FormDescription>
-                  Please enter your card information below.
-                </FormDescription>
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" placeholder="First Last" />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="number">Card number</Label>
-                <Input id="number" placeholder="" maxLength={16} />
-              </div>
-              <div className="grid grid-cols-3 gap-4">
-                <div className="grid gap-2">
-                  <Label htmlFor="month">Expires</Label>
-                  <Select>
-                    <SelectTrigger id="month">
-                      <SelectValue placeholder="Month" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="1">January</SelectItem>
-                      <SelectItem value="2">February</SelectItem>
-                      <SelectItem value="3">March</SelectItem>
-                      <SelectItem value="4">April</SelectItem>
-                      <SelectItem value="5">May</SelectItem>
-                      <SelectItem value="6">June</SelectItem>
-                      <SelectItem value="7">July</SelectItem>
-                      <SelectItem value="8">August</SelectItem>
-                      <SelectItem value="9">September</SelectItem>
-                      <SelectItem value="10">October</SelectItem>
-                      <SelectItem value="11">November</SelectItem>
-                      <SelectItem value="12">December</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-                <div className="grid gap-2">
-                  <Label htmlFor="year">Year</Label>
-                  <Select>
-                    <SelectTrigger id="year">
-                      <SelectValue placeholder="Year" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {Array.from({ length: 10 }, (_, i) => (
-                        <SelectItem
-                          key={i}
-                          value={`${new Date().getFullYear() + i}`}
-                        >
-                          {new Date().getFullYear() + i}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </div>
-                <div className="grid gap-2">
-                  <Label htmlFor="cvc">CVC</Label>
-                  <Input id="cvc" placeholder="CVC" maxLength={3} />
-                </div>
-              </div>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Update card</Button>
-      </form>
-    </Form>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-sm">
+            {error && <p data-cy='paymentApiError' className='text-red-500'>{error}</p>}
+            {successMsg && <p data-cy='paymentApiSuccess' className='text-green-500'>{successMsg}</p>}
+            <FormField
+                control={form.control}
+              name="name"
+              render={({ field }) => (
+                  <FormItem>
+                    <Label htmlFor="name">Name</Label>
+                    <Input data-cy="first-name" id="name" placeholder="First Last" {...field} />
+                    <FormMessage />
+                  </FormItem>
+              )}
+          />
+          <FormField
+              control={form.control}
+              name="number"
+              render={({ field }) => (
+                  <FormItem>
+                    <Label htmlFor="number">Card number</Label>
+                    <Input data-cy="card-number" id="number" placeholder="Card number" maxLength={16} {...field} />
+                    <FormMessage />
+                  </FormItem>
+              )}
+          />
+          <div className="grid grid-cols-3 gap-4">
+            <FormField
+                control={form.control}
+                name="month"
+                render={({ field }) => (
+                    <FormItem>
+                      <Label htmlFor="month">Month</Label>
+                      <Select onValueChange={field.onChange}>
+                        <SelectTrigger data-cy="month" id="month">
+                          <SelectValue placeholder="Month" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem role="option" value="1">January</SelectItem>
+                          <SelectItem role="option" value="2">February</SelectItem>
+                          <SelectItem role="option" value="3">March</SelectItem>
+                          <SelectItem role="option" value="4">April</SelectItem>
+                          <SelectItem role="option" value="5">May</SelectItem>
+                          <SelectItem role="option" value="6">June</SelectItem>
+                          <SelectItem role="option" value="7">July</SelectItem>
+                          <SelectItem role="option" value="8">August</SelectItem>
+                          <SelectItem role="option" value="9">September</SelectItem>
+                          <SelectItem role="option" value="10">October</SelectItem>
+                          <SelectItem role="option" value="11">November</SelectItem>
+                          <SelectItem role="option" value="12">December</SelectItem>
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                )}
+            />
+            <FormField
+                control={form.control}
+                name="year"
+                render={({ field }) => (
+                    <FormItem>
+                      <Label htmlFor="year">Year</Label>
+                      <Select onValueChange={field.onChange}>
+                        <SelectTrigger data-cy="year" id="year">
+                          <SelectValue placeholder="Year" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {Array.from({ length: 10 }, (_, i) => (
+                              <SelectItem
+                                  key={i}
+                                  value={`${new Date().getFullYear() + i}`}
+                              >
+                                {new Date().getFullYear() + i}
+                              </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                )}
+            />
+            <FormField
+                control={form.control}
+                name="cvc"
+                render={({ field }) => (
+                    <FormItem>
+                      <Label htmlFor="cvc">CVC</Label>
+                      <Input data-cy="cvc" id="cvc" placeholder="CVC" maxLength={3} {...field} />
+                      <FormMessage />
+                    </FormItem>
+                )}
+            />
+          </div>
+          <Button type="submit">Update card</Button>
+        </form>
+      </Form>
   );
 }
